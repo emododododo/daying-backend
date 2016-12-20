1eeba8f177227dcc05823ce9b21905eef3f717e4
smackxu <smackxu@raventech.com>
2016-12-20 16:31:38 +0800

    Temporary change dev ip

--- app/config.js
+++ app/config.js
@@ -4,9 +4,10 @@ var DEV = process.env.NODE_ENV === 'dev';
 
 config.express = {
   port: process.env.EXPRESS_PORT || 4007,
-  ip: '127.0.0.1'
+  // ip: '127.0.0.1'
+  ip: '0.0.0.0'
 };
 
-if (PRODUCTION) {
-  config.express.ip = '0.0.0.0'
-}
+// if (PRODUCTION) {
+//   config.express.ip = '0.0.0.0'
+// }
