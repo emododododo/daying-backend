88c92cb197523ea5b270202596e941c276caea9c
smackxu <smackxu@raventech.com>
2016-12-20 16:30:56 +0800

    Fixed some construction

--- app/api/resources/juejin.js
+++ app/api/resources/juejin.js
@@ -8,49 +8,53 @@
   solidot
 */
 const cheerio = require("cheerio");
-const iconv = require("iconv-lite");
-const charset = require('superagent-charset');
 const superagent = charset(require('superagent'));
 
 // 浏览器请求报文头部部分信息
+
 const browserMsg = {
   "Accept-Encoding": "gzip, deflate",
   "Origin": "http://e.xitu.io",
   "Referer": "http://e.xitu.io",
+  "Host": "api.xitu.io",
   "Content-Type": "application/json",
   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
 };
 const resourcesUrl = "http://api.xitu.io/resources/";
-const resourcesObj = {
-  github: 'github',
-  qianduan: 'qianduan',
-  cnblogs: 'cnblogs',
-  csdn: 'csdn',
-  wanqu: 'wanqu',
-  ithome: 'ithome',
-  solidot: 'solidot'
-}
-const data = {
 
+const data = {
+  "category": "trending",
+  "period": "day",
+  "lang": "javascript",
+  "offset": 0,
+  "limit": 30
 }
-function getJuejin(name, callback) {
-  const url = `http://api.xitu.io/resources/${name}`;
+
+function getJuejin(params, callback) {
+  const url = resourcesUrl + params.name;
   superagent
     .post(url)
-    .send({
-      "category": "trending",
-      "period": "day",
-      "lang": "javascript",
-      "offset": 0,
-      "limit": 30
-    })
+    .send(data)
     .set(browserMsg)
-    .end((err, response, body) => {
-      if (err) {
-        console.log(err);
+    .end((error, response, body) => {
+      const result = {};
+      if (error) {
+        result.error = error;
       } else {
-        callback(response);
+        result.data = JSON.parse(response.text).data;
+        if (params.name != 'github') {
+          result.data = result.data.reduce((arr, item) => {
+            arr.push({
+              url: item.url,
+              date: item.date.iso,
+              title: item.title
+            });
+            return arr;
+          }, []);
+        }
       }
+
+      callback(result);
     });
 }
 exports.getJuejin = getJuejin;
--- app/api/resources/router.js
+++ app/api/resources/router.js
@@ -1,26 +1,38 @@
-// var course = require('./edu-course');
 const log = require('bole')('customers/router');
 const express = require('express');
 const router = new express.Router();
 const getJuejin = require('./juejin').getJuejin;
+const dailyZhihu =  require('./dailyZhihu').dailyZhihu;
 
-const juejinResources = {
-  github: 'github',
-  qianduan: 'qianduan',
-  cnblogs: 'cnblogs',
-  csdn: 'csdn',
-  wanqu: 'wanqu',
-  ithome: 'ithome',
-  solidot: 'solidot'
+const resources = {
+  github: getJuejin,
+  qianduan: getJuejin,
+  cnblogs: getJuejin,
+  csdn: getJuejin,
+  wanqu: getJuejin,
+  ithome: getJuejin,
+  solidot: getJuejin,
+
+  dailyZhihu: dailyZhihu,
 }
 function handler(req, res) {
-  console.log(req.query.name);
-  // juejin api
-  if (juejinResources[req.query.name]) {
-    getJuejin(req.query.name, (result) => {
-      res.json(JSON.parse(result.text));
+  const getResource = resources[req.query.name];
+
+  if (getResource) {
+    getResource({
+      name: req.query.name
+    }, (result) => {
+      if (result.error) {
+        res.status(500).send(result.error);
+      } else {
+        res.json(result.data);
+      }
     });
+  } else {
+    // error
+    res.status(500).send('not found resource!!!');
   }
+
 }
 
 router.get('/', handler);
