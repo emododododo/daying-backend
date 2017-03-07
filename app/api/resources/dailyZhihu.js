eef136bb541146737fb5cf424ee9452af9a82695
smackxu <smackxu@raventech.com>
2017-03-07 19:01:24 +0800

    fixed eslint delete unuseful code

--- app/api/resources/dailyZhihu.js
+++ app/api/resources/dailyZhihu.js
@@ -1,62 +1,31 @@
-const cheerio = require("cheerio");
 const superagent = require('superagent');
 const moment = require('moment');
+
 moment.locale('zh-cn');
 // 浏览器请求报文头部部分信息
 const browserMsg = {
-  "Accept-Encoding": "gzip, deflate",
-  "Content-Type": "text/html; charset=UTF-8",
-  "Host": "daily.zhihu.com",
-  "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+  'Accept-Encoding': 'gzip, deflate',
+  'Content-Type': 'text/html; charset=UTF-8',
+  Host: 'daily.zhihu.com',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
 };
 
-// function dailyZhihu(params, callback) {
-//   superagent
-//     .get("http://daily.zhihu.com")
-//     .set(browserMsg)
-//     .end((error, response, body) => {
-//       const result = {};
-//       if (error) {
-//         result.error = error
-//       } else {
-//         const body = response.text;
-//         const $ = cheerio.load(body);
-//
-//         result.data = [];
-//         const itemList = $('.main-content-wrap a');
-//         itemList.each(function(index) {
-//           const url = 'http://daily.zhihu.com' + $(this).attr('href');
-//           const title = $(this).children('span').text();
-//           result.data.push({
-//             url,
-//             title
-//           });
-//         });
-//       }
-//
-//       callback(result);
-//     });
-// }
-// get dailyZhihuThemes list
-
 function dailyZhihuThemes() {
-  const promise = new Promise((resolve, reject) => {
+  const promise = new Promise((resolve) => {
     superagent
-      .get("http://news-at.zhihu.com/api/4/themes")
+      .get('http://news-at.zhihu.com/api/4/themes')
       .set(browserMsg)
-      .end((error, response, body) => {
+      .end((error, response) => {
         if (error) {
-          console.log(error);
+          console.error(error);
         } else {
-          const themes = JSON.parse(response.text).others.map((item) => {
-            return {
-              title: item.name,
-              id: `dailyZhihu_theme_${item.id}`,
-            }
-          });
+          const themes = JSON.parse(response.text).others.map(item => ({
+            title: item.name,
+            id: `dailyZhihu_theme_${item.id}`,
+          }));
           resolve(themes);
         }
-    });
+      });
   });
   return promise;
 }
@@ -80,26 +49,6 @@ function dailyZhihuSections() {
     id: 'dailyZhihu_section_38',
   }];
   return sections;
-  // const promise = new Promise((resolve) => {
-  //   superagent
-  //     .get("http://news-at.zhihu.com/api/3/sections")
-  //     .set(browserMsg)
-  //     .end((error, response, body) => {
-  //       const result = {};
-  //       if (error) {
-  //         console.log(error);
-  //       } else {
-  //         const sections = JSON.parse(response.text).data.map((item) => {
-  //           return {
-  //             title: item.name,
-  //             id: `dailyZhihu_section_${item.id}`,
-  //           }
-  //         });
-  //         resolve(sections);
-  //       }
-  //   });
-  // });
-  // return promise;
 }
 
 // getDailyZhihuList (需要添加失败情况)
@@ -121,10 +70,19 @@ function getDailyZhihuList() {
       values.forEach((item) => {
         dailyZhihuList.data = dailyZhihuList.data.concat(item);
       });
-      resolve([dailyZhihuList])
+      resolve([dailyZhihuList]);
     });
   });
   return promise;
+}
+
+function superagentHandler(url, handler) {
+  superagent
+    .get(url)
+    .set(browserMsg)
+    .end((error, response, body) => {
+      handler(error, response, body);
+    });
 }
 
 // handler dailyZhihu api
@@ -135,67 +93,52 @@ function dailyZhihu(params, callback) {
   switch (params.name.split('_')[1]) {
     case 'section':
       url = `http://news-at.zhihu.com/api/4/section/${params.name.split('_')[2]}`;
-      superagentHandler(url, (error, response, body) => {
+      superagentHandler(url, (error, response) => {
         const result = {};
         if (error) {
           result.error = error;
         } else {
-          result.data = JSON.parse(response.text).stories.map((item) => {
-            return {
-              url: `${storyUrl}${item.id}`,
-              title: item.title,
-              date: moment(item.date, 'YYYYMMDD').format('MMMDo'),
-            }
-          });
+          result.data = JSON.parse(response.text).stories.map(item => ({
+            url: `${storyUrl}${item.id}`,
+            title: item.title,
+            date: moment(item.date, 'YYYYMMDD').format('MMMDo'),
+          }));
         }
         callback(result);
       });
       break;
     case 'theme':
       url = `http://news-at.zhihu.com/api/3/theme/${params.name.split('_')[2]}`;
-      superagentHandler(url, (error, response, body) => {
+      superagentHandler(url, (error, response) => {
         const result = {};
         if (error) {
           result.error = error;
         } else {
-          result.data = JSON.parse(response.text).stories.map((item) => {
-            return {
-              url: `${storyUrl}${item.id}`,
-              title: item.title,
-              date: moment().format('MMMDo'),
-            }
-          });
+          result.data = JSON.parse(response.text).stories.map(item => ({
+            url: `${storyUrl}${item.id}`,
+            title: item.title,
+            date: moment().format('MMMDo'),
+          }));
         }
         callback(result);
       });
       break;
     default:
       url = 'http://news-at.zhihu.com/api/4/news/latest';
-      superagentHandler(url, (error, response, body) => {
+      superagentHandler(url, (error, response) => {
         const result = {};
         if (error) {
           result.error = error;
         } else {
-          result.data = JSON.parse(response.text).stories.map((item) => {
-            return {
-              url: `${storyUrl}${item.id}`,
-              title: item.title,
-              date: moment(JSON.parse(response.text).date, 'YYYYMMDD').format('MMMDo'),
-            }
-          });
+          result.data = JSON.parse(response.text).stories.map(item => ({
+            url: `${storyUrl}${item.id}`,
+            title: item.title,
+            date: moment(JSON.parse(response.text).date, 'YYYYMMDD').format('MMMDo'),
+          }));
         }
         callback(result);
       });
-  };
-}
-
-function superagentHandler(url, handler) {
-  superagent
-    .get(url)
-    .set(browserMsg)
-    .end((error, response, body) => {
-      handler(error, response, body);
-    });
+  }
 }
 
 exports.dailyZhihu = dailyZhihu;
--- app/api/resources/router.js
+++ app/api/resources/router.js
@@ -1,12 +1,12 @@
-const log = require('bole')('customers/router');
 const express = require('express');
+
 const router = new express.Router();
 const getJuejin = require('./juejin').getJuejin;
 const dailyZhihu = require('./dailyZhihu').dailyZhihu;
 const jianshu = require('./jianshu').jianshu;
 const smzdm = require('./jianshu').smzdm;
 const getList = require('./getList').getList;
-const qqSports= require('./qqSports').qqSports;
+const qqSports = require('./qqSports').qqSports;
 
 const resources = {
   github: getJuejin,
@@ -23,28 +23,27 @@ const resources = {
   qqSports,
 
   getList,
-}
+};
+
 function handler(req, res) {
   const getResource = resources[req.query.name.split('_')[0]];
 
   if (getResource) {
     getResource({
-      name: req.query.name
+      name: req.query.name,
     }, (result) => {
       if (result.error) {
         res.status(501).send(result);
       } else {
-        result.data = result.data;
         res.json(result);
       }
     });
   } else {
     // error
     res.status(501).send({
-      error: "Not found this resource"
+      error: 'Not found this resource',
     });
   }
-
 }
 
 router.get('/', handler);
