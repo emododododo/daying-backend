626a8a4154502378daa62a8cef10acc867db08c5
smackxu <smackxu@raventech.com>
2017-03-08 10:55:32 +0800

    fixed jianshu api eslint

--- app/api/resources/jianshu.js
+++ app/api/resources/jianshu.js
@@ -6,32 +6,32 @@ const superagent = require('superagent');
 
 function jianshu(params, callback) {
   const browserMsg = {
-    "Accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
-    "Accept-Encoding": "gzip, deflate, sdch",
-    "Host": "www.jianshu.com",
-    "Referer": "http://www.jianshu.com/",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+    Accept: 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    Host: 'www.jianshu.com',
+    Referer: 'http://www.jianshu.com/',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
   };
   superagent
-    .get("http://www.jianshu.com/trending/now?_=" + Date.now())
+    .get(`http://www.jianshu.com/trending/now?_=${Date.now()}`)
     .set(browserMsg)
-    .end((error, response, body) => {
+    .end((error, response) => {
       const result = {};
       if (error) {
-        result.error = error
+        result.error = error;
       } else {
         const body = response.text;
         const $ = cheerio.load(body);
 
         result.data = [];
         const itemList = $('.article-list .have-img');
-        itemList.each(function(index) {
+        itemList.each(function handler() {
           const $tT = $(this).find('.title a');
-          const url = 'http://www.jianshu.com' + $tT.attr('href');
+          const url = `http://www.jianshu.com${$tT.attr('href')}`;
           const title = $tT.text();
           result.data.push({
             url,
-            title
+            title,
           });
         });
       }
